50b3f9745c98ee4d0f1376cce1e4b269375ff2d6
danielolamide-byu <olamidedaniel874@gmail.com>
2025-05-27 19:54:43 +0100

    Fractions.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -8,11 +8,16 @@ class Program
         Console.WriteLine(fraction.GetFractionString());
         Console.WriteLine(fraction.GetDecimalValue());
 
-        Fraction fraction1 = new Fraction(3, 4);
+        Fraction fraction1 = new Fraction(5);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+
+
+        Fraction fraction2 = new Fraction(3, 4);
         Console.WriteLine(fraction1.GetFractionString());
         Console.WriteLine(fraction1.GetDecimalValue());
 
-        Fraction fraction2 = new Fraction(1, 3);
+        Fraction fraction3 = new Fraction(1, 3);
         Console.WriteLine(fraction2.GetFractionString());
         Console.WriteLine(fraction2.GetDecimalValue());
     }
